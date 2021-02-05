90f4d1ffb5eae624e412552006c4add895955d5b
Jai Karvir <jaikarvir@IS-5159s-MacBook-Pro.local>
2021-02-05 11:59:06 -0800

    first commit

--- vars/errorAndExceptionHandling.groovy
+++ vars/errorAndExceptionHandling.groovy
@@ -5,6 +5,7 @@ def call(artifactName="abc", startTime="",endTime=""){
     def errorAndExceptionHandling = libraryResource 'ErrorAndExceptionalHandling.js'
        writeFile file: "ErrorAndExceptionHandling.js", text: errorAndExceptionHandling  
     sh "npm install";
+    sh "npm install splunk-sdk"
     def result  = sh(returnStdout: true, script: "node ErrorAndExceptionHandling.js ${artifactName} ${startTime} ${endTime}").split("\r?\n")    
        println("result: " + apiResult);
 
