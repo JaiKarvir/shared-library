1d37533ba721d5d2e56850746d7517b1fdc44550
Jai Karvir <jaikarvir@IS-5159s-MacBook-Pro.local>
2021-04-12 13:15:00 -0700

    added qtest

--- vars/qtestbaseddod.groovy
+++ vars/qtestbaseddod.groovy
@@ -1,5 +1,7 @@
 def call(){
     println("Inside qtest based groovy file")
-     def result  = sh(returnStdout: true, script: "node Qtest_based_check.js").split("\r?\n")    
+    def qtest_based_check = libraryResource 'Qtest_based_check.js'
+       writeFile file: "Qtest_based_check.js", text: qtest_based_check  
+    def result  = sh(returnStdout: true, script: "node Qtest_based_check.js").split("\r?\n")    
     println("result: " + result);
 }
