95f63aa35660f093e2bad95a9a084af3604fd04e
Jai Karvir <jaikarvir@IS-5159s-MacBook-Pro.local>
2021-04-12 13:38:03 -0700

    added qtest

--- vars/qtestbaseddod.groovy
+++ vars/qtestbaseddod.groovy
@@ -2,6 +2,7 @@ def call(){
     println("Inside qtest based groovy file")
     def qtest_based_check = libraryResource 'Qtest_based_check.js'
        writeFile file: "Qtest_based_check.js", text: qtest_based_check  
+    sh "npm install";
     def result  = sh(returnStdout: true, script: "node Qtest_based_check.js").split("\r?\n")    
     println("result: " + result);
 }
