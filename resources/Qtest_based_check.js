02e60a3dad4f0e6e3ed182dc495eb5ad537ef1c0
Jai Karvir <jaikarvir@IS-5159s-MacBook-Pro.local>
2021-04-12 13:07:37 -0700

    added qtest

---
+++ resources/Qtest_based_check.js
@@ -0,0 +1,6 @@
+function qtest_handler(){
+    console.log("Inside js file")
+
+}
+
+qtest_handler();
--- vars/qtestbaseddod.groovy
+++ vars/qtestbaseddod.groovy
@@ -1,3 +1,5 @@
 def call(){
     println("Inside qtest based groovy file")
+     def result  = sh(returnStdout: true, script: "node Qtest_based_check.js").split("\r?\n")    
+    println("result: " + result);
 }
