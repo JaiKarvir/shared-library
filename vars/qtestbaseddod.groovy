30c0d8f1a85f27f0a8fc585d44963c8f6cd59913
Jai Karvir <jaikarvir@IS-5159s-MacBook-Pro.local>
2021-04-12 13:39:04 -0700

    added qtest

--- vars/qtestbaseddod.groovy
+++ vars/qtestbaseddod.groovy
@@ -3,6 +3,7 @@ def call(){
     def qtest_based_check = libraryResource 'Qtest_based_check.js'
        writeFile file: "Qtest_based_check.js", text: qtest_based_check  
     sh "npm install";
+    sh "npm install axios"
     def result  = sh(returnStdout: true, script: "node Qtest_based_check.js").split("\r?\n")    
     println("result: " + result);
 }
