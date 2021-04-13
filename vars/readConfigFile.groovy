afd9946b9973ba867a62df22ed402012aa06dabb
Jai Karvir <jaikarvir@IS-5159s-MacBook-Pro.local>
2021-04-13 15:16:20 -0700

    message

--- vars/readConfigFile.groovy
+++ vars/readConfigFile.groovy
@@ -3,6 +3,8 @@ def call(){
     def readConfig = libraryResource 'readConfig.js'
     writeFile file: "readConfig.js", text: readConfig
 
+    sh "npm install";
+    
     def result  = sh(returnStdout: true, script: "node readConfig.js").split("\r?\n") 
     println(result)
 }
