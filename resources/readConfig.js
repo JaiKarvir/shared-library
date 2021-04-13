f15cb5db5baa008e2b881cf01431290744fee09b
Jai Karvir <jaikarvir@IS-5159s-MacBook-Pro.local>
2021-04-13 15:21:09 -0700

    message

--- resources/readConfig.js
+++ resources/readConfig.js
@@ -1,5 +1,28 @@
-function readConfig(){
+async function readConfig(){
     console.log("Inside readConfig");
+    let configFile = await getDodConfigFileDetails("./DodConfig");
+    console.log(configFile);
 }
+
+function getDodConfigFileDetails(filename) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, 'utf8', (err, result) => {    
+            if (err) {
+                reject("Dod Config File not found");
+            }
+            if (result.length > 0) {
+                result = yaml.load(result);
+                if (result !== null) {
+                    resolve(result);
+                } else {
+                    reject("Dod Config file is empty")
+                }
+            } else {
+                reject("Dod Config File is empty")
+            }
+        })
+    })
+  }
+  
 
 readConfig();
