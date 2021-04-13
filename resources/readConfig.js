4ace79bb0caadca31bf1644e760095ea7c649c62
Jai Karvir <jaikarvir@IS-5159s-MacBook-Pro.local>
2021-04-13 15:30:16 -0700

    message

--- resources/readConfig.js
+++ resources/readConfig.js
@@ -3,11 +3,39 @@ const yaml = require('js-yaml');
 
 async function readConfig(){
     console.log("Inside readConfig");
-    let configFile = await getDodConfigFileDetails("/DodConfig");
+    let configFile = await getDodConfigFileDetails("./DodConfig");
     console.log(configFile);
 }
 
+
 function getDodConfigFileDetails(filename) {
+    return new Promise((resolve, reject) => {
+
+        try{
+        const dodFileContent = fs.readFileSync(filename, 'utf-8');
+        console.log(dodFileContent)
+        }catch{
+            reject("Dod Config File not found");
+        }
+        /*fs.readFile(filename, 'utf8', (err, result) => {    
+            if (err) {
+                reject("Dod Config File not found");
+            }
+            if (result.length > 0) {
+                result = yaml.load(result);
+                if (result !== null) {
+                    resolve(result);
+                } else {
+                    reject("Dod Config file is empty")
+                }
+            } else {
+                reject("Dod Config File is empty")
+            }
+        })*/
+    })
+  }
+
+function getDodConfigFileDetails1(filename) {
     return new Promise((resolve, reject) => {
         fs.readFile(filename, 'utf8', (err, result) => {    
             if (err) {
