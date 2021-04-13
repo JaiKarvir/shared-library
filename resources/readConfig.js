9ff51ee7049fb0f9ed0cc70cd03dd7b0cd62ea75
Jai Karvir <jaikarvir@IS-5159s-MacBook-Pro.local>
2021-04-13 15:36:51 -0700

    message

--- resources/readConfig.js
+++ resources/readConfig.js
@@ -5,7 +5,6 @@ async function readConfig(){
     console.log("Inside readConfig");
     try{
     let configFile = await getDodConfigFileDetails("./DodConfig");
-
     console.log(configFile);
     }catch(error){
         console.log(error);
@@ -13,34 +12,28 @@ async function readConfig(){
 }
 
 
-function getDodConfigFileDetails1(filename) {
+function getDodConfigFileDetails(filename) {
     return new Promise((resolve, reject) => {
 
         try{
         const dodFileContent = fs.readFileSync(filename, 'utf-8');
-        console.log(dodFileContent)
+        if (dodFileContent.length > 0) {
+            result = yaml.load(dodFileContent);
+            if (result !== null) {
+                resolve(result);
+            } else {
+                reject("Dod Config file is empty")
+            }
+        } else {
+            reject("Dod Config File is empty")
+        }
         }catch{
             reject("Dod Config File not found");
         }
-        /*fs.readFile(filename, 'utf8', (err, result) => {    
-            if (err) {
-                reject("Dod Config File not found");
-            }
-            if (result.length > 0) {
-                result = yaml.load(result);
-                if (result !== null) {
-                    resolve(result);
-                } else {
-                    reject("Dod Config file is empty")
-                }
-            } else {
-                reject("Dod Config File is empty")
-            }
-        })*/
     })
   }
 
-function getDodConfigFileDetails(filename) {
+function getDodConfigFileDetails1(filename) {
     return new Promise((resolve, reject) => {
         fs.readFileSync(filename, 'utf8', (err, result) => {    
             if (err) {
